b9a4b106c4cbed06d92aa37da7520a74f59e942c
Scott Vanderlind <scottv@ifixit.com>
2014-07-28 19:54:13 -0700

    Add some error checking
    So when we have iOS devices enrolled too we don't try to message
    them with the GCM provider.

--- app/controllers/api/v1/hate_controller.rb
+++ app/controllers/api/v1/hate_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::HateController < ApiController
       # This is the controller where the magic happens.
       # We accept a list of a destination user and an insult id
       # and then send a push notification.
+      # NOTE: This assumes the device is Android
       user = api_user
 
       GCM.key = "AIzaSyA8_Rt7vvplk3gR8YrQwPjKJ-5QAyGvj8U"
@@ -10,14 +11,21 @@ class Api::V1::HateController < ApiController
       begin
          target = User.find(params[:target_user]);
          insult = Insult.find(params[:insult_id]);
-         device = target.devices.first
+         devices = target.devices
       rescue
          api_response(400, "invalid parameters") and return
       end
 
       data = { :title => user.username, :message => insult.body }
 
-      GCM.send_notification(device.token, data)
+      # Send it to all of their devices, because why not?
+      devices.each do |device|
+         if device.device_type.eql? "ANDROID"
+            GCM.send_notification(device.token, data)
+         end
+         if device.device_type.eql? "IOS"
+         end
+      end
 
    end
 end
