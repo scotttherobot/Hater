a6b0ba196812f70f95cd4d081cf1c10362867b06
Scott Vanderlind <scottv@ifixit.com>
2014-07-30 20:22:37 -0700

    Destroy existing devices before enrolling
    So if you change accounts on a device, the token gets assigned
    to the more recent login.

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -3,6 +3,14 @@ class Api::V1::DevicesController < ApiController
    ## Add a new push subscription
    def create
       user = api_user
+      # Let's first look for existing devices with the same token. IF they
+      # are there, let's delete them and assume that a new user logged in
+      # on the device.
+      oldDevice = Device.find_by_token(params[:token])
+      if oldDevice
+         oldDevice.destroy
+      end
+
       @device = Device.new(:user => user, :device_type => params[:device_type], :token => params[:token])
       api_response(400, "device failed to register") and return unless @device.save
    end
