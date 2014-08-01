c46194e15be508c91fa99ed65e906a607a9bdfae
Scott Vanderlind <scottv@ifixit.com>
2014-08-01 16:54:22 -0700

    Make push notifications work for IOS!
    Yay for iOS push!!

--- app/controllers/api/v1/hate_controller.rb
+++ app/controllers/api/v1/hate_controller.rb
@@ -6,7 +6,13 @@ class Api::V1::HateController < ApiController
       # NOTE: This assumes the device is Android
       user = api_user
 
+      # Set up GCM
       GCM.key = "AIzaSyA8_Rt7vvplk3gR8YrQwPjKJ-5QAyGvj8U"
+
+      # Setup APNS
+      APNS.host = "gateway.sandbox.push.apple.com"
+      APNS.pem = "/home/scott/rails/h8r/certs/ck.pem"
+      APNS.pass = "anncoulter"
 
       begin
          target = User.find(params[:target_user]);
@@ -17,13 +23,14 @@ class Api::V1::HateController < ApiController
       end
 
       data = { :title => user.username, :message => "says " + insult.body }
-
       # Send it to all of their devices, because why not?
       devices.each do |device|
          if device.device_type.eql? "ANDROID"
             GCM.send_notification(device.token, data)
          end
          if device.device_type.eql? "IOS"
+            alert = user.username + " says " + insult.body
+            APNS.send_notification(device.token, {:alert => alert, :sound => "default"})
          end
       end
 
---
+++ certs/readme.txt
@@ -0,0 +1,5 @@
+Hello!
+
+This is where we keep our certs, like for SSL and for push notifications and
+stuff like that.  The entire contents of the folder is gitignored (except 
+this file, of course).  This is here becasue git won't track an empty folder.
